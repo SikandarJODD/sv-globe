265d2e6e3cbd59f405798ab5cd683b215d553259
SikandarJODD <sikandar6174@gmail.com>
2026-06-04 14:50:09 +0530

    feat: simple hero

---
+++ src/lib/components/landing/hero.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import ChevronRight from '@lucide/svelte/icons/chevron-right';
+</script>
+
+<main class="mx-auto mt-8 max-w-5xl text-center lg:text-left">
+	<h1
+		class="bg-linear-to-b from-blue-500 to-ink dark:from-white dark:to-neutral-700 bg-clip-text font-gp-circle text-5xl  text-balance text-transparent md:text-6xl lg:mt-16 xl:text-5xl"
+	>
+		Svelte Globe Examples
+	</h1>
+	<p class="mt-8 max-w-2xl font-figtree text-lg text-balance text-muted-foreground">
+		Provides Setup Guide & 10 Examples using <a
+			href="https://cobe.vercel.app"
+			target="_blank"
+			class="text-primary underline underline-offset-2">Cobe</a
+		>.
+	</p>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Hero from '$lib/components/landing/hero.svelte';
+</script>
+
+<Hero />
